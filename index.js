35fd045a911246f1d3f0969e96d7c0b7b3f80a51
Md Saimun Hossain <saimonanam@gmail.com>
2019-04-08 11:17:45 +0600

    homepage setup done

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const path = require('path')
 const express = require('express')
 
 const app = new express()
+
+app.use(express.static('public'))
+
+app.get('/',(req, res) => {
+    res.sendFile(path.resolve(__dirname,'pages/index.html'))
+})
 
 app.listen(3000, () => {
     console.log('App Listening 3000')
