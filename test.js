617e8dc8270b8978f68fb5e5cd32bf747b71a60c
Md Saimun Hossain <saimonanam@gmail.com>
2019-04-08 15:36:27 +0600

    read data from database

--- test.js
+++ test.js
@@ -5,10 +5,14 @@ const Post = require('./database/models/Post')
 
 mongoose.connect('mongodb://localhost:27017/NodeBlog',{ useNewUrlParser: true } )
 
-Post .create({
-    title: 'My first blog post',
-    description: 'Blog post description',
-    content: 'Lorem ipsum content is here'
-}, (error, post) => {
-    console.log(error, post)
+Post.find({}, (error, posts) => {
+    console.log(error, posts)
 })
+
+// Post .create({
+//     title: 'My third blog post',
+//     description: 'Blog third post description',
+//     content: 'Lorem ipsum content is here third'
+// }, (error, post) => {
+//     console.log(error, post)
+// })
