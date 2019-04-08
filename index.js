3908125996bfe5c87a8b5b265bd5863d10bd6dab
Md Saimun Hossain <saimonanam@gmail.com>
2019-04-08 12:26:03 +0600

    edge templating engine with express has been added to project & implemented to all through pages

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const path = require('path')
+const expressEdge = require('express-edge')
 const express = require('express')
 
 const app = new express()
 
 app.use(express.static('public'))
+app.use(expressEdge)
+app.set('views',`${__dirname}/views`)
 
 app.get('/',(req, res) => {
-    res.sendFile(path.resolve(__dirname,'pages/index.html'))
+    res.render('index')
 })
 app.get('/about',(req, res) => {
-    res.sendFile(path.resolve(__dirname,'pages/about.html'))
+    res.render('about')
 })
 app.get('/post',(req, res) => {
-    res.sendFile(path.resolve(__dirname,'pages/post.html'))
+    res.render('post')
 })
 app.get('/contact',(req, res) => {
-    res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+    res.render('contact')
 })
 
 app.listen(3000, () => {
