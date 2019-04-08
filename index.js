447f76a16630720704f02b9837f632b18a1e3004
Md Saimun Hossain <saimonanam@gmail.com>
2019-04-08 11:31:50 +0600

    added rest of the pages

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ app.use(express.static('public'))
 app.get('/',(req, res) => {
     res.sendFile(path.resolve(__dirname,'pages/index.html'))
 })
+app.get('/about',(req, res) => {
+    res.sendFile(path.resolve(__dirname,'pages/about.html'))
+})
+app.get('/post',(req, res) => {
+    res.sendFile(path.resolve(__dirname,'pages/post.html'))
+})
+app.get('/contact',(req, res) => {
+    res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+})
 
 app.listen(3000, () => {
     console.log('App Listening 3000')
