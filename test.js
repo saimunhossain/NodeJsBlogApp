e7bbb7b997d59d1750d5082856a0ecaa9a6571d8
Md Saimun Hossain <saimonanam@gmail.com>
2019-04-08 15:54:42 +0600

    data has been updated by id

--- test.js
+++ test.js
@@ -5,8 +5,18 @@ const Post = require('./database/models/Post')
 
 mongoose.connect('mongodb://localhost:27017/NodeBlog',{ useNewUrlParser: true } )
 
-Post.find({}, (error, posts) => {
-    console.log(error, posts)
+// Post.find({}, (error, posts) => {
+//     console.log(error, posts)
+// })
+
+// Post.findById("5cab0b68fd19a11944d375e1", (error, post) => {
+//     console.log(error, post)
+// })
+
+Post.findByIdAndUpdate('5cab0b68fd19a11944d375e1', {
+    title: 'My first blog post has been updated'
+},(error, post) => {
+    console.log(error, post)
 })
 
 // Post .create({
