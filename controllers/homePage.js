7e7b2d608394f29774410f5aba6d4c5ed60af3c8
Md Saimun Hossain <saimonanam@gmail.com>
2019-04-10 23:07:16 +0600

    refactoring codes & convert it complete MVC pattern

---
+++ controllers/homePage.js
@@ -0,0 +1,8 @@
+const Post = require('../database/models/Post')
+module.exports = async(req, res) => {
+    const posts = await Post.find({})
+    console.log(posts)
+    res.render('index', {
+        posts
+    })
+}
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const path = require('path')
 const expressEdge = require('express-edge')
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
 
-const Post = require('./database/models/Post')
+const createPostController = require('./controllers/createPost')
+const homePageController = require('./controllers/homePage')
+const storePostController = require('./controllers/storePost')
+const getPostController = require('./controllers/getPost')
 
 const app = new express()
 
@@ -28,40 +30,11 @@ const validateCreatePostMiddleware = (req, res, next) => {
 
 app.use('/posts/store', validateCreatePostMiddleware)
 
-app.get('/', async(req, res) => {
-    const posts = await Post.find({})
-    console.log(posts)
-    res.render('index', {
-        posts
-    })
-})
-app.get('/posts/new',(req, res) => {
-    res.render('create')
-})
-app.post('/posts/store', (req, res) => {
-    const { image } = req.files
-    image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
-        Post.create({
-            ...req.body,
-            image: `/posts/${image.name}`
-        }, (error, post) => {
-            res.redirect('/')
-        })
-    })
-    
-})
-app.get('/about',(req, res) => {
-    res.render('about')
-})
-app.get('/post/:id', async(req, res) => {
-    const post = await Post.findById(req.params.id)
-    res.render('post', {
-        post
-    })
-})
-app.get('/contact',(req, res) => {
-    res.render('contact')
-})
+app.get('/', homePageController)
+app.get('/post/:id', getPostController)
+app.get('/posts/new', createPostController)
+app.post('/posts/store', storePostController)
+
 
 app.listen(3000, () => {
     console.log('App Listening 3000')
