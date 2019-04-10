5a9920b4c2f8105c47b32d67570bad222164ec2f
Md Saimun Hossain <saimonanam@gmail.com>
2019-04-10 19:10:54 +0600

    added database field to the schema

--- database/models/Post.js
+++ database/models/Post.js
@@ -4,7 +4,12 @@ const mongoose = require('mongoose')
 const PostSchema = new mongoose.Schema({
     title: String,
     description: String,
-    content: String
+    content: String,
+    username: String,
+    createdAt:{
+      type: Date,
+      default: new Date()  
+    }
 })
 
 const Post = mongoose.model('Post', PostSchema)
